4e8239a64e7b816f9b88b8f54d4a24cf36c239c7
Shaun Ho <sefho98@gmail.com>
2024-03-15 15:03:27 +0000

    Updating constructor test

--- tests/test_aircraft.cpp
+++ tests/test_aircraft.cpp
@@ -2,6 +2,6 @@
 #include <../include/aircraft.h>
 #include <../include/aircraft_data.h>
 TEST(AircrafTest, Construction){
-    // Aircraft my_aircraft;
+    Aircraft my_aircraft;
     ASSERT_EQ(1,1);
 }
