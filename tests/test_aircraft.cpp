caa4ffdcb63124bb256b3c591f1783c7919b9acb
Shaun Ho <sefho98@gmail.com>
2024-03-15 16:28:15 +0000

    Updating tests

--- tests/test_aircraft.cpp
+++ tests/test_aircraft.cpp
@@ -1,6 +1,9 @@
-#include <gtest/gtest.h>
 #include <../include/aircraft.h>
 #include <../include/aircraft_data.h>
-TEST(AircrafTest, Construction){
-    Aircraft my_aircraft;
+#include <gtest/gtest.h>
+
+
+TEST(AircrafTest, Construction) {
+  Aircraft my_aircraft;
+  ASSERT_GT(my_aircraft.mass, 0.0);
 }
