20802ea5f8878ac8f39b44b91bec362b377f95d1
Shaun Ho <sefho98@gmail.com>
2024-04-07 13:01:27 +0100

    Adding Matrix3x3 class

--- src/maths.cpp
+++ src/maths.cpp
@@ -25,6 +25,13 @@ Vector &Vector::operator+=(Vector u) {
   z += u.z;
   return *this;
 }
+
+Vector operator+(Vector u, Vector v) {
+  return Vector(u.x + v.x, u.y + v.y, u.z + v.z);
+}
+Vector operator-(Vector u, Vector v) {
+  return Vector(u.x - v.x, u.y - v.y, u.z - v.z);
+}
 Vector operator*(Vector u, double m) {
   return Vector(u.x * m, u.y * m, u.z * m);
 }
@@ -41,8 +48,70 @@ Vector operator/(double m, Vector u) {
 void Vector::normalise() {
   double magnitude;
   magnitude = sqrt(x * x + y * y + z * z);
-  std::cout << magnitude << std::endl;
   x /= magnitude;
   y /= magnitude;
   z /= magnitude;
 }
+
+Matrix3x3::Matrix3x3(void) {
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      data[i][j] = 0.0;
+    }
+  }
+  this->compute_and_set_determinant();
+};
+// clang-format off
+Matrix3x3::Matrix3x3(
+  double r1c1, double r1c2, double r1c3, 
+  double r2c1, double r2c2, double r2c3, 
+  double r3c1, double r3c2, double r3c3
+                      // clang-format on
+) {
+  data[0][0] = r1c1;
+  data[0][1] = r1c2;
+  data[0][2] = r1c3;
+  data[1][0] = r2c1;
+  data[1][1] = r2c2;
+  data[1][2] = r2c3;
+  data[2][0] = r3c1;
+  data[2][1] = r3c2;
+  data[2][2] = r3c3;
+  this->compute_and_set_determinant();
+};
+
+Matrix3x3::Proxy::Proxy(double *_data) : _data(_data) {}
+
+double &Matrix3x3::Proxy::operator[](int index) { return _data[index]; };
+
+Matrix3x3::Proxy Matrix3x3::operator[](int index) {
+  return Proxy(data[index]);
+};
+
+void Matrix3x3::compute_and_set_determinant() {
+  double local_determinant =
+      data[0][0] * (data[1][1] * data[2][2] - data[1][2] * data[2][1]) +
+      data[0][1] * (data[1][0] * data[2][2] - data[1][2] * data[0][2]) +
+      data[0][2] * (data[1][0] * data[2][1] - data[1][1] * data[2][0]);
+
+  determinant = (local_determinant == 0.0) ? 1.0 : local_determinant;
+};
+
+Matrix3x3 Matrix3x3::inverse() {
+  return Matrix3x3((data[1][1] * data[2][2] - data[1][2] * data[2][1]),
+                   -(data[0][1] * data[2][2] - data[0][2] * data[2][1]),
+                   (data[0][1] * data[1][2] - data[0][2] * data[1][1]),
+                   -(data[1][0] * data[2][2] - data[1][2] * data[2][0]),
+                   (data[0][0] * data[2][2] - data[0][2] * data[2][0]),
+                   -(data[0][0] * data[1][2] - data[0][2] * data[1][0]),
+                   (data[1][0] * data[2][1] - data[1][1] * data[2][0]),
+                   -(data[0][0] * data[2][1] - data[0][1] * data[2][0]),
+                   (data[0][0] * data[1][1] - data[0][1] * data[1][0])) /
+         this->determinant;
+};
+
+Matrix3x3 operator/(Matrix3x3 m, double s) {
+  return Matrix3x3(m[0][0] / s, m[0][1] / s, m[0][2] / s, m[1][0] / s,
+                   m[1][1] / s, m[1][2] / s, m[2][0] / s, m[2][1] / s,
+                   m[2][2] / s);
+};
--- tests/test_maths.cpp
+++ tests/test_maths.cpp
@@ -12,13 +12,31 @@ TEST(ConversionFunctions, RadiansToDegrees) {
   ASSERT_EQ(degrees, 180);
 };
 
-TEST(VectorOperations, VectorAddition) {
+TEST(VectorOperations, VectorAdditionEqual) {
   Vector vec_1 = Vector(1.0, 2.0, 3.0);
   Vector vec_2 = Vector(1.0, 2.0, 3.0);
   vec_1 += vec_2;
   ASSERT_EQ(vec_1.x, 2.0);
   ASSERT_EQ(vec_1.y, 4.0);
   ASSERT_EQ(vec_1.z, 6.0);
+};
+
+TEST(VectorOperations, VectorAddition) {
+  Vector vec_1 = Vector(2.0, 4.0, 6.0);
+  Vector vec_2 = Vector(1.0, 2.0, 3.0);
+  Vector vec_3 = vec_1 + vec_2;
+  ASSERT_EQ(vec_3.x, 3.0);
+  ASSERT_EQ(vec_3.y, 6.0);
+  ASSERT_EQ(vec_3.z, 9.0);
+};
+
+TEST(VectorOperations, VectorSubtraction) {
+  Vector vec_1 = Vector(2.0, 4.0, 6.0);
+  Vector vec_2 = Vector(1.0, 2.0, 3.0);
+  Vector vec_3 = vec_1 - vec_2;
+  ASSERT_EQ(vec_3.x, 1.0);
+  ASSERT_EQ(vec_3.y, 2.0);
+  ASSERT_EQ(vec_3.z, 3.0);
 };
 
 TEST(VectorOperations, VectorMultiplyByDouble) {
@@ -60,3 +78,55 @@ TEST(VectorOperations, VectorNormalisation) {
   ASSERT_EQ(vec_3.y, 0.0);
   ASSERT_EQ(vec_3.z, 0.0);
 }
+
+TEST(MatrixOperations, MatrixInitialisation) {
+  Matrix3x3 matrix(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
+};
+
+TEST(MatrixOperations, MatrixDivideByScalar) {
+  Matrix3x3 matrix(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
+  Matrix3x3 matrix2 = matrix / 2.0;
+  ASSERT_EQ(matrix2[0][0], 0.5);
+  ASSERT_EQ(matrix2[0][1], 1.0);
+  ASSERT_EQ(matrix2[0][2], 1.5);
+  ASSERT_EQ(matrix2[1][0], 2.0);
+  ASSERT_EQ(matrix2[1][1], 2.5);
+  ASSERT_EQ(matrix2[1][2], 3.0);
+  ASSERT_EQ(matrix2[2][0], 3.5);
+  ASSERT_EQ(matrix2[2][1], 4.0);
+  ASSERT_EQ(matrix2[2][2], 4.5);
+};
+
+TEST(MatrixOperations, MatrixGetElement) {
+  Matrix3x3 matrix(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
+  ASSERT_EQ(matrix[0][1], 2.0);
+};
+
+TEST(MatrixOperations, MatrixSetElement) {
+  Matrix3x3 matrix;
+  matrix[0][1] = 1.0;
+
+  ASSERT_EQ(matrix[0][0], 0.0);
+  ASSERT_EQ(matrix[0][1], 1.0);
+  ASSERT_EQ(matrix[0][2], 0.0);
+  ASSERT_EQ(matrix[1][0], 0.0);
+  ASSERT_EQ(matrix[1][1], 0.0);
+  ASSERT_EQ(matrix[1][2], 0.0);
+  ASSERT_EQ(matrix[2][0], 0.0);
+  ASSERT_EQ(matrix[2][1], 0.0);
+  ASSERT_EQ(matrix[2][2], 0.0);
+};
+TEST(MatrixOperations, MatrixInverse) {
+  Matrix3x3 matrix(0.0, -3.0, -2.0, 1.0, -4.0, -2.0, -3.0, 4.0, 1.0);
+  Matrix3x3 matrix_inversed = matrix.inverse();
+
+  ASSERT_EQ(matrix_inversed[0][0], 4.0);
+  ASSERT_EQ(matrix_inversed[0][1], -5.0);
+  ASSERT_EQ(matrix_inversed[0][2], -2.0);
+  ASSERT_EQ(matrix_inversed[1][0], 5.0);
+  ASSERT_EQ(matrix_inversed[1][1], -6.0);
+  ASSERT_EQ(matrix_inversed[1][2], -2.0);
+  ASSERT_EQ(matrix_inversed[2][0], -8.0);
+  ASSERT_EQ(matrix_inversed[2][1], 9.0);
+  ASSERT_EQ(matrix_inversed[2][2], 3.0);
+};
